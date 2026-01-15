1619336b516b388661eb4cfe55048d6cf012ebd1
joaquinbowen <bowenjoaquinsd@gmail.com>
2026-01-14 21:23:14 -0500

    Inicio Firebase

---
+++ ejercicios/FLUTTER/biblioteca copy/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:biblioteca/views/add_book_page.dart';
+import 'package:biblioteca/views/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+/*void main() {
+  runApp(const MainApp());
+}*/
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Biblioteca Demo",
+      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.lightBlue),
+      routes: {'/add': (context) => const AddBookPage()},
+    );
+  }
+}
